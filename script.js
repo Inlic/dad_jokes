1d983f99da1a00e04fda9771f900c79a9496a297
Ben Wiedeback <66647604+Inlic@users.noreply.github.com>
2020-12-11 16:28:22 -0700

    updated async code

--- script.js
+++ script.js
@@ -3,13 +3,29 @@ const jokeButton = document.getElementById('jokeBtn')
 
 generateJoke()
 
-function generateJoke() {
+// async architecture
+
+async function generateJoke() {
   const config = {
     headers: {
       'Accept': 'application/json'
     }
   }
-  fetch('https://icanhazdadjoke.com', config).then(res => res.json()).then((data) => {
-    jokeElement.innerHTML = data.joke
-  })
+  const res = await fetch('https://icanhazdadjoke.com', config)
+  const data = await res.json()
+  jokeElement.innerHTML = data.joke
 }
+
+
+// promise architecture 
+
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       'Accept': 'application/json'
+//     }
+//   }
+//   fetch('https://icanhazdadjoke.com', config).then(res => res.json()).then((data) => {
+//     jokeElement.innerHTML = data.joke
+//   })
+// }
